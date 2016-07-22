321f8f102d25f5e3b50699edad6909980b51a5ce
MAXIMYIIIKA <maximyiiika@mail.ru>
2016-07-22 17:13:36 +0300

    stuff 2

--- src/main/java/interfaces/Module.java
+++ src/main/java/interfaces/Module.java
@@ -1,8 +1,12 @@
 package interfaces;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by Max Nichipor on 10.07.2016.
  */
+
+@Component
 public interface Module {
     ModuleInfo getInfo();
 }
--- src/main/java/interfaces/ModuleInfo.java
+++ src/main/java/interfaces/ModuleInfo.java
@@ -1,8 +1,12 @@
 package interfaces;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by Max Nichipor on 22.07.2016.
  */
+
+@Component
 public interface ModuleInfo {
     String getName();
 }
